a1e2b5d411df56b2ebea9049e19d844146beb44a
Nicoline L. Thomsen - Drone Consulting <xnlth@energinet.dk>
2024-04-11 15:58:26 +0200

    Added addon source code

---
+++ src/blender/__init__.py
@@ -0,0 +1,214 @@
+import bpy
+from bpy.utils import register_class, unregister_class
+
+from .reachy_marionette import ReachyMarionette
+
+# Addon metadata
+bl_info = {
+    "name": "ReachyMarionette",
+            "author": "Drone Consulting",
+            "version": (1, 0, 0),
+            "blender": (4, 0, 1),
+            "location": "Toolbar > ReachyMarionette",
+            "description": "Connects to the Reachy robot from Pollen Robotics, to stream angles of joints in Reachy rig.",
+            "category": "Animation"
+}
+
+# Install missing packages to Blenders Python install
+try:
+    import reachy_sdk
+
+except:
+    import os
+    import pip
+    import platform
+    import subprocess
+    import sys
+
+    print("No reachy_sdk module found, installing with pip...")
+
+    if platform.system() == 'win32':
+        print("Installing on Windows...")
+        python_exe = os.path.join(sys.prefix, 'bin', 'python.exe')
+        target = os.path.join(sys.prefix, 'lib', 'site-packages')
+
+        subprocess.call([python_exe, '-m', 'ensurepip'])
+        subprocess.call(
+            [python_exe, '-m', 'pip', 'install', '--upgrade', 'pip'])
+
+        subprocess.call([python_exe, '-m', 'pip', 'install',
+                        '--upgrade', 'reachy-sdk', '-t', target])
+
+    else:
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", 'reachy-sdk'])
+
+    import reachy_sdk
+    print("Done installing reachy_sdk")
+
+
+# Global object
+reachy = ReachyMarionette()
+
+
+# Classes
+
+class SceneProperties(bpy.types.PropertyGroup):
+    # Defining custom properties to be used by the addon panel
+
+    def callback_kinematics(self, context):
+        # Toggle IK constraint on bones that has thems
+        scene_properties = context.scene.scn_prop
+
+        for bone in bpy.context.active_object.pose.bones:
+
+            if not 'IK' in bone.constraints:
+                continue
+
+            if scene_properties.Kinematics == 'FK':
+                bone.constraints['IK'].enabled = False
+            elif scene_properties.Kinematics == 'IK':
+                bone.constraints['IK'].enabled = True
+
+    IPaddress: bpy.props.StringProperty(
+        name="IP adress",
+        description="Reachy's IP adress (default = localhost)",
+        default="localhost")  # type: ignore (stops warning squiggles)
+
+    Kinematics: bpy.props.EnumProperty(
+        name="Kinematics",
+        description="Choose if rig is controlled by forward kinematics (FK) or inverse kinematics (IK).",
+        items=[('FK', 'FK', ''),
+               ('IK', 'IK', '')],
+        default='FK',
+        update=callback_kinematics)  # type: ignore (stops warning squiggles)
+
+
+class REACHYMARIONETTE_OT_ConnectReachy(bpy.types.Operator):
+    # Handling connection to Reachy
+    bl_idname = "object.connect_reachy"
+    bl_label = "Connect to Reachy Robot via IP-adress"
+
+    def execute(self, context):
+        scene_properties = context.scene.scn_prop
+
+        reachy.connect_reachy(self.report, scene_properties.IPaddress)
+
+        return {'FINISHED'}
+
+
+class REACHYMARIONETTE_OT_DisconnectReachy(bpy.types.Operator):
+    # Handling connection to Reachy
+    bl_idname = "object.disconnect_reachy"
+    bl_label = "Disconnect current connection to Reachy"
+
+    def execute(self, context):
+
+        reachy.disconnect_reachy(self.report)
+
+        return {'FINISHED'}
+
+
+class REACHYMARIONETTE_OT_SendPose(bpy.types.Operator):
+    # Get angles form Blender rig, and send to Reachy
+
+    bl_idname = "object.send_pose"
+    bl_label = "Send current pose once"
+
+    def execute(self, context):
+
+        reachy.send_angles(self.report)
+
+        return {'FINISHED'}
+
+
+class REACHYMARIONETTE_OT_StreamAngles(bpy.types.Operator):
+    # Continously get angles form Blender rig, and stream to Reachy
+
+    bl_idname = "object.stream_angles"
+    bl_label = "Live streaming of bone angles"
+
+    def __init__(self):
+        print("Stream starting...")
+
+    def __del__(self):
+        print("Stream ended")
+
+    def modal(self, context, event):
+        if event.type == 'ESC':
+            reachy.stream_angles_disable()
+
+            self.report({'INFO'}, "ESC key pressed, stopping stream")
+            return {'FINISHED'}
+
+        return {'PASS_THROUGH'}
+
+    def invoke(self, context, event):
+        context.window_manager.modal_handler_add(self)
+
+        reachy.stream_angles_enable(self.report)
+
+        return {'RUNNING_MODAL'}
+
+
+class REACHYMARIONETTE_PT_Panel(bpy.types.Panel):
+    # Addon panel displaying options
+
+    bl_label = "Stream Angles"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "ReachyMarionette"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def draw(self, context):
+        layout = self.layout
+        scene_properties = context.scene.scn_prop
+
+        layout.prop(scene_properties, "IPaddress")
+
+        layout.row().operator(REACHYMARIONETTE_OT_ConnectReachy.bl_idname,
+                              text="Connect to Reachy", icon='PLUGIN')
+
+        layout.row().operator(REACHYMARIONETTE_OT_DisconnectReachy.bl_idname,
+                              text="Disconnect Reachy", icon='UNLINKED')
+
+        layout.prop(scene_properties, "Kinematics")
+
+        layout.row().operator(REACHYMARIONETTE_OT_SendPose.bl_idname,
+                              text="Send Pose", icon='ARMATURE_DATA')
+
+        layout.row().operator(REACHYMARIONETTE_OT_StreamAngles.bl_idname,
+                              text="Stream Pose", icon='ARMATURE_DATA')
+
+
+classes = (
+    SceneProperties,
+    REACHYMARIONETTE_OT_ConnectReachy,
+    REACHYMARIONETTE_OT_DisconnectReachy,
+    REACHYMARIONETTE_OT_SendPose,
+    REACHYMARIONETTE_OT_StreamAngles,
+    REACHYMARIONETTE_PT_Panel
+)
+
+
+def register():
+    for cls in classes:
+        register_class(cls)
+
+    bpy.types.Scene.scn_prop = bpy.props.PointerProperty(type=SceneProperties)
+
+
+def unregister():
+    for cls in (classes):
+        unregister_class(cls)
+
+    del bpy.types.Scene.scn_prop
+
+    def temp(_x, _y):
+        ...
+
+    reachy.disconnect_reachy(temp)
+
+
+if __name__ == "__main__":
+    register()
---
+++ src/blender/reachy_marionette.py
@@ -0,0 +1,174 @@
+import mathutils
+import numpy as np
+import functools
+
+import bpy
+from reachy_sdk import ReachySDK
+from reachy_sdk.reachy_sdk import flush_communication
+from reachy_sdk.trajectory import goto
+from reachy_sdk.trajectory.interpolation import InterpolationMode
+
+
+class ReachyMarionette():
+
+    def __init__(self):
+        self.reachy = None
+        self.is_streaming = False
+
+    # Helper functions from rigify plugin
+    def get_pose_matrix_in_other_space(self, mat, pose_bone):
+        """ Returns the transform matrix relative to pose_bone's current
+        transform space. In other words, presuming that mat is in
+        armature space, slapping the returned matrix onto pose_bone
+        should give it the armature-space transforms of mat.
+        TODO: try to handle cases with axis-scaled parents better.
+        """
+        rest = pose_bone.bone.matrix_local.copy()
+        rest_inv = rest.inverted()
+
+        if pose_bone.parent.name != "Root":
+            par_mat = pose_bone.parent.matrix.copy()
+            par_inv = par_mat.inverted()
+            par_rest = pose_bone.parent.bone.matrix_local.copy()
+        else:
+            par_mat = mathutils.Matrix()
+            par_inv = mathutils.Matrix()
+            par_rest = mathutils.Matrix()
+
+        # Get matrix in bone's current transform space
+        smat = rest_inv @ (par_rest @ (par_inv @ mat))
+
+        return smat
+
+    def get_bones_rotation(self, pose_bone, axis):
+
+        mat = self.get_pose_matrix_in_other_space(pose_bone.matrix, pose_bone)
+
+        if axis == 0:
+            return mat.to_euler().x
+        elif axis == 1:
+            return mat.to_euler().y
+        elif axis == 2:
+            return mat.to_euler().z
+
+    def angle_of_bone(self, name):
+
+        bone = bpy.context.object.pose.bones[name]
+        # Get unconstrained axis
+        axis_rot = (np.array(bone.lock_rotation) == False).nonzero()[0][0]
+
+        return np.rad2deg(self.get_bones_rotation(bone, axis_rot))
+
+    def connect_reachy(self, report_function, ip='localhost'):
+
+        if self.reachy != None:
+            report_function(
+                {'INFO'}, "Connection already established at '%s'" % ip)
+            return
+
+        # Try connection
+        try:
+            self.reachy = ReachySDK(host=ip)
+            self.reachy.turn_on('reachy')
+            report_function({'INFO'}, "Connection established succesfully!")
+
+        except:
+            report_function(
+                {'ERROR'}, ("Could not find connection at '%s'" % ip))
+
+    def disconnect_reachy(self, report_function):
+
+        # Try connection
+        if self.reachy != None:
+            self.reachy_reset_pose()
+            # self.reachy.turn_off_smoothly('reachy')
+            # flush_communication()
+            report_function({'WARNING'}, "Proper disconnection disabled!")
+            self.reachy = None
+            report_function({'INFO'}, "Disconnected Reachy")
+
+        else:
+            report_function({'INFO'}, "No Reachy is connected")
+
+    def send_angles(self, report_function):
+
+        if self.reachy == None:
+            report_function({'ERROR'}, "Reachy not connected!")
+            return
+
+        if bpy.context.object.type != 'ARMATURE':
+            report_function({'ERROR'}, "Please select Armature")
+            return
+
+        joint_angle_positions = {
+            # Right arm
+            self.reachy.r_arm.r_shoulder_pitch: self.angle_of_bone("shoulder_pitch.R"),
+            self.reachy.r_arm.r_shoulder_roll: self.angle_of_bone("shoulder_roll.R"),
+            self.reachy.r_arm.r_arm_yaw: self.angle_of_bone("shoulder_yaw.R"),
+            self.reachy.r_arm.r_elbow_pitch: self.angle_of_bone("elbow_pitch.R"),
+            self.reachy.r_arm.r_forearm_yaw: self.angle_of_bone("forearm_yaw.R"),
+            self.reachy.r_arm.r_wrist_pitch: self.angle_of_bone("wrist_pitch.R"),
+            self.reachy.r_arm.r_wrist_roll: self.angle_of_bone("wrist_roll.R"),
+            self.reachy.r_arm.r_gripper: self.angle_of_bone("gripper.R"),
+            # Left arm
+            self.reachy.l_arm.l_shoulder_pitch: self.angle_of_bone("shoulder_pitch.L"),
+            self.reachy.l_arm.l_shoulder_roll: self.angle_of_bone("shoulder_roll.L"),
+            self.reachy.l_arm.l_arm_yaw: self.angle_of_bone("shoulder_yaw.L"),
+            self.reachy.l_arm.l_elbow_pitch: self.angle_of_bone("elbow_pitch.L"),
+            self.reachy.l_arm.l_forearm_yaw: self.angle_of_bone("forearm_yaw.L"),
+            self.reachy.l_arm.l_wrist_pitch: self.angle_of_bone("wrist_pitch.L"),
+            self.reachy.l_arm.l_wrist_roll: self.angle_of_bone("wrist_roll.L"),
+            self.reachy.l_arm.l_gripper: self.angle_of_bone("gripper.L"),
+        }
+
+        goto(
+            goal_positions=joint_angle_positions,
+            duration=1.0,
+            interpolation_mode=InterpolationMode.MINIMUM_JERK
+        )
+
+    def stream_angles(self, report_function):
+        if self.is_streaming:
+            self.send_angles(report_function)
+            # TODO: magic number! (seconds till next function call)
+            return 2.0
+        else:
+            return None
+
+    def stream_angles_enable(self, report_function):
+        self.is_streaming = True
+
+        # Create Blender timer
+        bpy.app.timers.register(functools.partial(
+            self.stream_angles, report_function))
+
+    def stream_angles_disable(self):
+        self.is_streaming = False
+
+    def reachy_reset_pose(self):
+        joint_angle_positions = {
+            # Right arm
+            self.reachy.r_arm.r_shoulder_pitch: 0,
+            self.reachy.r_arm.r_shoulder_roll: 0,
+            self.reachy.r_arm.r_arm_yaw: 0,
+            self.reachy.r_arm.r_elbow_pitch: 0,
+            self.reachy.r_arm.r_forearm_yaw: 0,
+            self.reachy.r_arm.r_wrist_pitch: 0,
+            self.reachy.r_arm.r_wrist_roll: 0,
+            self.reachy.r_arm.r_gripper: 0,
+            # Left arm
+            self.reachy.l_arm.l_shoulder_pitch: 0,
+            self.reachy.l_arm.l_shoulder_roll: 0,
+            self.reachy.l_arm.l_arm_yaw: 0,
+            self.reachy.l_arm.l_elbow_pitch: 0,
+            self.reachy.l_arm.l_forearm_yaw: 0,
+            self.reachy.l_arm.l_wrist_pitch: 0,
+            self.reachy.l_arm.l_wrist_roll: 0,
+            self.reachy.l_arm.l_gripper: 0
+        }
+
+        goto(
+            goal_positions=joint_angle_positions,
+            duration=1.0,
+            interpolation_mode=InterpolationMode.MINIMUM_JERK
+        )
